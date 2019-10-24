7bade88e5e32a94b16c6cb72a3586dd41ce68f03
Erandkings <Erandkings12@gmail.com>
2019-10-24 19:40:52 +0100

    add Map & Phone Intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -26,6 +26,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 // TODO - Task 3 - Launch the Map Activity
 // ***
+
     public void createMapIntent(View view) {
         Uri uriMatch = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriMatch);
@@ -38,6 +39,7 @@ public class InfoActivity extends AppCompatActivity {
     // ***
 // TODO - Task 3 - Launch the Phone Activity
 // ***
+
     public void createPhoneIntent(View view) {
         Uri uriPhone = Uri.parse("tel:0123456789");
         Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
